1cd47a43017b1444d3648f1393b565b3750ea39d
bb <brady.ben12@gmail.com>
2021-01-10 21:57:18 -0800

    updates for week 4

--- find_plan.py
+++ find_plan.py
@@ -65,7 +65,7 @@ def find_best_remaining_plan(weeks):
 
 
 if __name__ == "__main__":
-    weeks = nba_schedule.load_weeks('data/nba_predictions_2021_01_04.csv', '2021-01-04')
+    weeks = nba_schedule.load_weeks('data/nba_predictions_2021_01_11.csv', '2021-01-11')
     best_plan = find_best_remaining_plan(weeks)
     print(best_plan.plan)
     print(best_plan.record.wins, best_plan.record.games)
